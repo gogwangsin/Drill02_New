f75700ed9c08631f40a5d57daeef62ec4928f7fd
gogwangsin <gwangsin1029@naver.com>
2023-09-21 09:58:20 +0900

    기본적인 무한루프가 완성

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -6,4 +6,17 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def run_circle():
+    pass
+def run_rectangle():
+    pass
+
+
+
+while True:
+    run_circle()
+    run_rectangle()
+
+
+
 close_canvas()
