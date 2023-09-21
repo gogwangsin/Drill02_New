a0a72f09b28b6f319b1e28a491dddb3430d4c76f
gogwangsin <gwangsin1029@naver.com>
2023-09-21 10:41:17 +0900

    왼쪽 상단에서 내려가는 함수 idle 확인 후 완성

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -33,11 +33,15 @@ def run_rectangle():
 
 #    for x in range(50,750 + 1,10):
 #        render_frame(x,90)
-    for y in range(90, 550+1, 10):
-         render_frame(750,y)
+
+#    for y in range(90, 550+1, 10):
+#         render_frame(750,y)
     
 #    for x in range(750,50-1,-10):
 #        render_frame(x, 550)
+
+    for y in range(550, 90-1, -10):
+         render_frame(50,y)
         
     pass
 
