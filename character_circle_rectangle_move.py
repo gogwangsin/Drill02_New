c69fb180d39b7402d74cb25546fe9bb42c95c955
gogwangsin <gwangsin1029@naver.com>
2023-09-21 10:35:23 +0900

    함수화가 제대로 됐다

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -8,38 +8,41 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def render_frame(x,y):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    delay(0.01)
+
+
+
+
 def run_circle():
     print('Circle')
 
     # 일단 그림을 그리자
     cx, cy, r = 400, 300, 200
+    
     for degree in range(0,360,1):
         x = cx + r * math.cos(math.radians(degree))
         y = cy + r * math.sin(math.radians(degree))
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.01)
+        render_frame(x,y)
 
 def run_rectangle():
     print('Rectangle')
 
     for x in range(50,750 + 1,10):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.01)
+        render_frame(x,90)
 
-
-
-
-    
+#    for x in range(750,50-1,-10):
+#        render_frame(x, 90)
+        
     pass
 
 
 
 while True:
-    #run_circle()
+    run_circle()
     run_rectangle()
    # run_circle()
     break
